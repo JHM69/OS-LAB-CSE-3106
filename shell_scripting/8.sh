7520e1d378bfb9e4c786c265d2a9d967291cedc6
jhm69 <jahangirhmarup@gmail.com>
2023-08-23 02:41:11 +0600

    update

--- shell_scripting/8.sh
+++ shell_scripting/8.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "Enter the directory name: "
-read dirname
-find "$dirname" -type d -empty -delete
+echo "Enter a number: "
+read num
+factorial=1
+for ((i = 1; i <= num; i++)); do
+  factorial=$((factorial * i))
+done
+echo "The factorial of $num is: $factorial"
